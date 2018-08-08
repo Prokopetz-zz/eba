829d9edc5f88b4160ddc70dcd0b311e402e499fe
Nicolas Prokopetz <nicolas@prokopetz.com.br>
2018-08-08 09:57:31 -0300

    Make routes with consign

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 var server = require('./config/server');
 
-var routeAbout = require('./app/routes/about')(server);
-var routeHome = require('./app/routes/home')(server);
-
  
 
 server.listen(3000, function () {

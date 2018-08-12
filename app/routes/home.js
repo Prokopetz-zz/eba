8731702c3bba0b9328479288a26bb087389b6d78
Nicolas Prokopetz <nicolas@prokopetz.com.br>
2018-08-11 21:47:10 -0300

    It adds sass

--- app/routes/home.js
+++ app/routes/home.js
@@ -3,7 +3,6 @@ module.exports = function(server){
   server.get('/', function(req, res){
     //.config.database is the arquives names /config/database.js
     var database = server.config.database();
-
     res.render('home/home')
   });
 }

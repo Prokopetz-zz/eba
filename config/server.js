74bcb28c128ae3f67f84aa1f607645f53d4398bb
Nicolas Prokopetz <nicolas@prokopetz.com.br>
2018-08-11 23:41:17 -0300

    Fix css files to be able to link on html

--- config/server.js
+++ config/server.js
@@ -1,8 +1,11 @@
+var express = require('express');
 var server = require('express')();
 var consign = require('consign')();
 
 server.set('view engine', 'ejs');
 server.set('views', './app/views');
+server.use(express.static('./public/'));
+
 
 consign.include('app/routes').then('config/database.js').into(server);
 
